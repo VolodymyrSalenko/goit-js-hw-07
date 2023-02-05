a0f013ac3cdbc1d1f620e4397603e6e985cb1eab
VolodymyrSalenko <vova.salenko@gmail.com>
2023-02-05 02:35:29 +0200

    Task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryContent = galleryItems.map(({ preview, original, description }) => {
+return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    
+}).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryContent);
+
+
+const gallery = new SimpleLightbox('.gallery a', {   
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
